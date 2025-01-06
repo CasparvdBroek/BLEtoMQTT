e0666d7319a832951bd296bebcbe843f1eaa7483
Caspar van den Broek <csprvndnbrk@gmail.com>
2025-01-06 17:49:52 +1300

    setting up

--- README.md
+++ README.md
@@ -4,12 +4,17 @@
 
 For each BLE device detected BLEtoMQTT will discover all exposed services and characteristics creating MQTT topics for each. Those characteristics which are writeable will automatically be 'subscribed' from the MQTT broker.
 
+Two external libraries are used :
+	- org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.2.5
+	- com.github.weliem:blessed-android
+
+The author uses this application to connect remote custom ESP32 devices to his Home Assistant server via a Zerotier VPN. 
+The GitHub repository contains an example setup.json and a prebuilt apk used to monitor/control a boat on the mooring or at anchor.
+
  The app has been tried on Samsung S9, S9+ and 'Galaxy Tab A'. Running Android 10 and 11. 
 ## Installation
 Clone repository and open in Android IDE. Build apk and install on your device.
 Using an Android file manager create a new directory in the user root called 'bletomqtt' (/mnt/sdcard/bletomqtt) in which to place the setup.json file.
-
-The GitHub repository contains an example setup.json and a prebuilt apk.
 
 Go in to Settings->App->BLEtoMQTT and grant location and file privileges
 
--- app/src/main/java/com/CasparvdBroek/bletomqtt/BLEtoMqttService.java
+++ app/src/main/java/com/CasparvdBroek/bletomqtt/BLEtoMqttService.java
@@ -4,12 +4,41 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
-public class BLEtoMqttService extends Service {
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BLEtoMqttService extends Service {
+    JSONObject jsonObj ;
+    JSONArray jsonArray ;
+    JSONObject mqttJsonObj ;
+    JSONArray bleJsonArr;
     @Override
     public void onCreate() {
         super.onCreate();
+        try {
+            jsonObj = new JSONObject(readFileFromSDCard());
+            jsonArray = jsonObj.getJSONArray("bletomqtt");
+            for (int i = 0; i < jsonArray.length(); i++) {
+                if (jsonArray.getJSONObject(i).has("mqtt")) {
+                    mqttJsonObj = jsonArray.getJSONObject(i).getJSONObject("mqtt");
+                }
+                if (jsonArray.getJSONObject(i).has("devices")) {
+                    bleJsonArr = jsonArray.getJSONObject(1).getJSONArray("devices");
+                }
+            }
 
+        } catch (Exception e) {
+        }
+
+//Starts the Bluetooth scanning, callbacks run in UI thread (ie main)
+        BluetoothHandler.getInstance(this,bleJsonArr);
+
+        MQTTHandler.getInstance(this,mqttJsonObj);
 
     }
 
@@ -27,4 +56,44 @@ public class BLEtoMqttService extends Service {
         return null;
     }
 
+    private String readFileFromSDCard() {
+
+        File file = new File("/sdcard/bletomqtt","setup.json");
+        if (!file.exists()) {
+            throw new RuntimeException("File not found");
+        }
+
+        BufferedReader reader = null;
+        StringBuilder builder = null;
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            builder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return builder.toString();
+    }
+
+    public int two2power(int exponent){
+        if (exponent == 0 ){
+            return 1;
+        }
+        if (exponent > 15  ){
+            return 0;
+        }
+        return 1 << exponent;
+
+    }
 }
