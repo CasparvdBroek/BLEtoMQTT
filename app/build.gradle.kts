f0fdf4516ce07c0aeb24276692201f996fb3e10b
Caspar van den Broek <csprvndnbrk@gmail.com>
2025-04-02 07:47:43 +1300

    Signed release version v1.02
    ICON changed and first signed release version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,18 +11,22 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "1"
+        versionName = "1.02"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
+            isDebuggable = false
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+        }
+        debug {
+            isDebuggable = true
         }
     }
     compileOptions {
