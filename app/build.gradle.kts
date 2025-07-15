b7968f5600f170ab9cbf4b0851239eb72e2d60ad
Caspar van den Broek <csprvndnbrk@gmail.com>
2025-07-15 16:38:41 +1200

    v1.03.01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.CasparvdBroek.bletomqtt"
         minSdk = 24
         targetSdk = 29
-        versionCode = 4
-        versionName = "1.02.03"
+        versionCode = 5
+        versionName = "1.03.01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
